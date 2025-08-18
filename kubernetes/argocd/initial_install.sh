2bbeb4b4d8a9d9f60221543a3eeb73af76d7a8c5
Dustin D <dustin@Dustins-MacBook-Pro-2.local>
2025-08-17 23:33:16 -0700

    chore: add initial scripts

---
+++ kubernetes/argocd/initial_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl create namespace argocd
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+
